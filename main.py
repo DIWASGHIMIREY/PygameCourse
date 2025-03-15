dd307d416558f9faba0c666bc33c0dc8cb412328
26ghimireyd <26ghimireyd@bwschools.net>
2025-03-15 15:17:53 -0400

    Final commit, the game is fully fleshed out (as much as I feel like doing)
    Signed-off-by: Syn <95244272+SynnLM@users.noreply.github.com>

--- main.py
+++ main.py
@@ -19,8 +19,11 @@ YELLOW = (231,200,100)
 PURPLE = (113, 52, 235)
 IDK = (23,143,251)
 
+mob_spawn_delay = 600
+
 running = True
 game_over = False
+boss_fight = False
 
 
 font_name = p.font.match_font("arial")
@@ -43,6 +46,59 @@ def newmob():
 def draw_lives(surf, x, y, lives, icon):
     for i in range(lives):
         surf.blit(icon, (x + 35 * i, y))
+
+class Boss(p.sprite.Sprite):
+    def __init__(self):
+        p.sprite.Sprite.__init__(self)
+        self.height = 120
+        self.width = 120
+        # self.image_list = []
+        self.lives = 50
+
+
+        self.last_shot = p.time.get_ticks()
+
+        self.shoot_delay = 200
+
+        self.explosion_sound_list = []
+        for i in range(1, 10):
+            self.sound_file = p.mixer.Sound(f'All Files/Audio/Explosions/boom{i}.wav')
+            self.explosion_sound_list.append(self.sound_file)
+
+        for sound in self.explosion_sound_list:
+            sound.set_volume(0.05)
+
+        self.image = p.image.load("All Files/boss.png")
+        self.image = p.transform.scale(self.image, (self.width,self.height))
+
+        self.rect = self.image.get_rect()
+
+        self.rect.x = random.randint(0, WIDTH - self.width)
+        self.rect.y = random.randint(-50, -10)
+        self.speedy = 3
+        self.speedx = 8
+    def shoot(self):
+        if self.now - self.last_shot > self.shoot_delay:
+            self.last_shot = self.now
+            boss_bullet = Bullet(self.rect.centerx,self.rect.bottom, random.randint(-3,3))
+            boss_bullet.speedy *= -1
+            all_sprites.add(boss_bullet)
+            boss_bullets.add(boss_bullet)
+            shoot_sound.play()
+
+
+    def update(self):
+        self.now = p.time.get_ticks()
+        self.rect.y += self.speedy
+        if self.rect.y >= 70:
+            self.speedy = 0
+            self.rect.x += self.speedx
+            if self.rect.left <= 0:
+                self.speedx *= -1
+            elif self.rect.right >= WIDTH:
+                self.speedx *= -1
+            self.shoot()
+
 
 class Explosion(p.sprite.Sprite):
     def __init__(self, center, size):
@@ -158,6 +214,9 @@ class Player(p.sprite.Sprite):
         self.width = 45
         self.pw = [1,2,3,4]
 
+        self.boss_kill = 0
+        self.total_boss_count = 2
+
         # self.image = p.Surface((self.width,self.height))
         # self.image.fill(WHITE)
 
@@ -196,8 +255,8 @@ class Player(p.sprite.Sprite):
         self.lvl = 1
         # Powerups
         self.bullet_dmg = 1
-        self.speedup = True
-        self.dual_shot = True
+        self.speedup = False
+        self.dual_shot = False
         self.refilling = False
 
     def shoot(self):
@@ -211,42 +270,44 @@ class Player(p.sprite.Sprite):
         if self.now - self.last_shot > self.shoot_delay:
             self.last_shot = self.now
             if self.dual_shot:
-                b1 = Bullet(self.rect.centerx - 10, self.rect.top, -1)
-                b2 = Bullet(self.rect.centerx + 10, self.rect.top, 1)
-                b1.image = p.image.load("All Files/Powerups/bullet_dual.png")
-                b2.image = p.image.load("All Files/Powerups/bullet_dual.png")
-                all_sprites.add(b1, b2)
-                bullets.add(b1, b2)
-                self.mana -= 5
-                if self.mana <= 0:
+                if self.mana >= 5:
+                    b1 = Bullet(self.rect.centerx - 10, self.rect.top, -1)
+                    b2 = Bullet(self.rect.centerx + 10, self.rect.top, 1)
+                    b1.image = p.image.load("All Files/Powerups/bullet_dual.png")
+                    b2.image = p.image.load("All Files/Powerups/bullet_dual.png")
+                    all_sprites.add(b1, b2)
+                    bullets.add(b1, b2)
+                    self.mana -= 5
+                    if self.speedup and self.dual_shot:
+                        b1.image = p.image.load("All Files/Powerups/bullet_dual_speedup.png")
+                        b2.image = p.image.load("All Files/Powerups/bullet_dual_speedup.png")
+                elif self.mana < 5:
+                    self.mana = 0
                     self.dual_shot = False
                     self.refilling = True
-                    self.mana = 0
+
 
                 self.ammo -= 2
                 shoot_sound.play()
+
 
             else:
                 bullet = Bullet(self.rect.centerx,self.rect.top, 0)
                 all_sprites.add(bullet)
                 bullets.add(bullet)
                 if self.speedup:
-                    bullet.image = p.image.load("All Files/Powerups/bullet_speed.png")
+                    bullet.image = p.image.load("All Files/Powerups/bullet_speedup.png")
                 self.ammo -= 1
                 shoot_sound.play()
             if self.speedup:
-                self.shoot_delay = 150
-                self.mana -= 2
-                if self.mana <= 0:
-                    self.mana = 0
+                if self.mana >= 2:
+                    self.shoot_delay = 150
+                    self.mana -= 2
+                elif self.mana < 2:
                     self.speedup = False
                     self.refilling = True
             else:
                 self.shoot_delay = 350
-
-
-
-
 
     def update(self):
         self.now = p.time.get_ticks()
@@ -272,14 +333,18 @@ class Player(p.sprite.Sprite):
                 if self.mana >= MAX_MANA:
                     self.mana = MAX_MANA
                     self.refilling = False
+                return
+
+            if self.mana <= MAX_MANA:
+                self.speedup = False
+                self.dual_shot = False
 
         if self.reloading:
             if self.now - self.last_reload > self.reload_time:
                 self.ammo = self.max_ammo
-                self.last_reload = self.now
                 self.reloading = False
+                self.last_reload = self.now
             return
-
 
 class Mob(p.sprite.Sprite):
     def __init__(self):
@@ -300,10 +365,6 @@ class Mob(p.sprite.Sprite):
         self.image = p.image.load("All Files/enemy.png")
         self.image = p.transform.scale(self.image, (self.width,self.height))
 
-        # for i in range(1,11):
-        #     filename = p.image.load(f'All Files/Mob Sprites/meteor{i}.png')
-        #     self.image_list.append(filename)
-        # self.image = random.choice(self.image_list)
         self.rect = self.image.get_rect()
 
         self.rect.x = random.randint(0, WIDTH - self.width)
@@ -318,6 +379,9 @@ class Mob(p.sprite.Sprite):
             plr.score += 1
             plr.total_score += 1
             self.kill()
+        elif plr.lvl == 5 or plr.lvl == 10:
+            plr.score = 0
+
 
 class Power(p.sprite.Sprite):
     def __init__(self,x,y):
@@ -339,7 +403,7 @@ class Power(p.sprite.Sprite):
         self.rect.center = (x,y)
         # self.image.fill(WHITE)
 
-        self.spawn_percent = 0.1
+        self.spawn_percent = 0.2
         self.spawn = False
 
         self.ran = random.random()
@@ -355,6 +419,10 @@ class Power(p.sprite.Sprite):
         self.rect.y += self.speedy
         if self.rect.y > HEIGHT:
             self.kill()
+        if plr.counter == 2:
+            self.spawn_percent += 0.1
+            plr.counter = 0
+            print(f"Percent: {self.spawn_percent}")
 
 def start_screen():
     screen.fill(BLACK)
@@ -377,6 +445,8 @@ def start_screen():
 
 def level_up():
     plr.score = 0
+    plr.counter += 1
+    print(f"Count: {plr.counter}")
     screen.fill(BLACK)
     draw_text(screen, "Level up!", 64, WIDTH // 2, (HEIGHT // 2.5) - 20, LIGHT_BLUE)
     draw_text(screen, f"Level {plr.lvl}", 32, WIDTH // 2, (HEIGHT // 2.5) + 55, LIGHT_BLUE)
@@ -391,6 +461,7 @@ def level_up():
         all_sprites.empty()
         meteors.empty()
         bullets.empty()
+        boss_bullets.empty()
         for event in p.event.get():
             if event.type == p.QUIT:
                 p.quit()
@@ -399,7 +470,56 @@ def level_up():
             all_sprites.add(plr)
             last = now
 
+
+
+p.init()
+p.mixer.init()
+
+screen = p.display.set_mode((WIDTH, HEIGHT))
+p.display.set_caption("Space Invaders")
+
+icon = p.image.load("All Files/gameicon.png")
+p.display.set_icon(icon)
+
+clock = p.time.Clock()
+
+start_screen()
+
+all_sprites = p.sprite.Group()
+
+meteors = p.sprite.Group()
+bosses = p.sprite.Group()
+powerups = p.sprite.Group()
+bullets = p.sprite.Group()
+boss_bullets = p.sprite.Group()
+
+plr = Player()
+
+all_sprites.add(plr)
+
+status_bar = StatusBar(plr)
+
+mob_during_boss = 900
+mob_delay_decrease = 0
+last_meteor_spawn = p.time.get_ticks()
+
+background = p.image.load('All Files/background.png')
+background = p.transform.scale(background, (WIDTH, HEIGHT))
+
+background_rect = background.get_rect()
+p.mixer.music.load('All Files/Audio/background_audio.wav')
+p.mixer.music.play(-1)
+
+shoot_sound = p.mixer.Sound('All Files/Audio/laser_fire.wav')
+
+life_icon = p.image.load('All Files/player.png')
+life_icon = p.transform.scale(life_icon, (30, 30))
+
+shoot_sound.set_volume(0.05)
+p.mixer.music.set_volume(0.05)
+
 def boss_lvl():
+    global boss_fight, bosses, game_over
     screen.fill(BLACK)
     if plr.lvl == 5:
         draw_text(screen, "Boss Level 1", 64, WIDTH // 2, (HEIGHT // 2.5) - 20, RED)
@@ -418,64 +538,36 @@ def boss_lvl():
         bullets.empty()
 
         for event in p.event.get():
-            k = p.key.get_pressed()
             if event.type == p.QUIT:
                 p.quit()
-
-            if k[p.K_c]:
-                waiting = False
-                all_sprites.add(plr)
-            elif k[p.K_v]:
-                waiting = False
-                if plr.lvl != 11:
-                    level_up()
-
-p.init()
-p.mixer.init()
-
-screen = p.display.set_mode((WIDTH, HEIGHT))
-p.display.set_caption("Space Invaders")
-
-icon = p.image.load("All Files/gameicon.png")
-p.display.set_icon(icon)
-
-clock = p.time.Clock()
-
-start_screen()
-
-all_sprites = p.sprite.Group()
-
-meteors = p.sprite.Group()
-powerups = p.sprite.Group()
-bullets = p.sprite.Group()
-
-plr = Player()
-
-all_sprites.add(plr)
-
-status_bar = StatusBar(plr)
-
-meteor_spawn_delay = 600
-last_meteor_spawn = p.time.get_ticks()
-
-background = p.image.load('All Files/background.png')
-background = p.transform.scale(background, (WIDTH, HEIGHT))
-
-background_rect = background.get_rect()
-p.mixer.music.load('All Files/Audio/background_audio.wav')
-p.mixer.music.play(-1)
-
-shoot_sound = p.mixer.Sound('All Files/Audio/laser_fire.wav')
-
-life_icon = p.image.load('All Files/player.png')
-life_icon = p.transform.scale(life_icon, (30, 30))
-
-shoot_sound.set_volume(0.05)
-p.mixer.music.set_volume(0.05)
+            elif event.type == p.KEYDOWN:
+                if event.key == p.K_c:
+                    waiting = False
+                    bosses.empty()
+                    boss = Boss()
+                    bosses.add(boss)
+                    all_sprites.add(boss)
+                    all_sprites.add(plr)
+                    boss_fight = True
+                elif event.key == p.K_v:
+                    waiting = False
+                    boss_fight = False
+                    if plr.lvl != 11:
+                        all_sprites.empty()
+                        meteors.empty()
+                        bullets.empty()
+                        boss_bullets.empty()
+                        bosses.empty()
+                        all_sprites.add(plr)
+                        plr.lvl += 1
+                        if plr.lvl >= 11:
+                            game_over = True
+                        level_up()
 
 while running:
     clock.tick(FPS)
     current_time = p.time.get_ticks()
+
     for event in p.event.get():
         if event.type == p.QUIT:
             running = False
@@ -506,11 +598,12 @@ while running:
         draw_text(screen, "GAME OVER", 64, WIDTH // 2, HEIGHT // 2.5, RED)
         draw_text(screen, "Press SPACE to Restart", 32, WIDTH // 2, (HEIGHT // 2.5) - 25, PURPLE)
         draw_text(screen, "Press Q to Quit", 32, WIDTH // 2, (HEIGHT // 2.5) + 60, PURPLE)
-        draw_text(screen, f"You killed {plr.total_score} mobs!", 32, WIDTH // 2, (HEIGHT // 2.5) + 100, YELLOW)
+        draw_text(screen, f"You killed {plr.total_score} total mobs!", 32, WIDTH // 2, (HEIGHT // 2.5) + 100, YELLOW)
+        draw_text(screen, f"You managed to beat {plr.boss_kill} out of {plr.total_boss_count} bosses!", 32, WIDTH // 2, (HEIGHT // 2.5) + 150, YELLOW)
         p.display.flip()
 
     else:
-        if current_time - last_meteor_spawn > meteor_spawn_delay:
+        if current_time - last_meteor_spawn > mob_spawn_delay:
             newmob()
             last_meteor_spawn = current_time
 
@@ -522,8 +615,10 @@ while running:
         hit_player =  p.sprite.spritecollide(plr, meteors, True)
         hit_mob = p.sprite.groupcollide(meteors, bullets, False, True)
         hit_powers = p.sprite.spritecollide(plr, powerups, True)
+        hit_boss = p.sprite.groupcollide(bosses,bullets,False, True)
+        boss_hit_player = p.sprite.spritecollide(plr,boss_bullets,True)
 
-
+        # If hit
         if hit_player:
             expl = Explosion(plr.rect.midtop,'sm')
             all_sprites.add(expl)
@@ -559,9 +654,13 @@ while running:
                     plr.ammo = plr.max_ammo
                 elif plr.pwup == 2:
                     plr.dual_shot = True
+                    if plr.mana >= 5:
+                        plr.refilling = False
                     plr.bullet_dmg = 2
                 elif plr.pwup == 3:
                     plr.speedup = True
+                    if plr.mana >= 2:
+                        plr.refilling = False
                 elif plr.pwup == 4:
                     plr.health += 10
                     if plr.health >= MAX_HP:
@@ -569,25 +668,61 @@ while running:
 
 
 
+        if boss_hit_player:
+            expl = Explosion(plr.rect.midtop,'sm')
+            all_sprites.add(expl)
+            plr.health -= 10
+            if plr.health <= 0:
+                plr.health = MAX_HP
+                plr.lives -= 1
+                if plr.lives <= 0:
+                    game_over = True
+
+        if hit_boss:
+            for boss in hit_boss:
+                expl = Explosion(boss.rect.center,'lg')
+                all_sprites.add(expl)
+                boss.lives -= 0.5
+                if boss.lives <= 0:
+                    boss.kill()
+                    boss_fight = False
+                    plr.lvl += 1
+                    plr.total_score += 1
+                    plr.boss_kill += 1
+                    if plr.lvl != 11:
+                        level_up()
+                    elif plr.lvl >= 11:
+                        game_over = True
+
+
+        # Powers
         if plr.score >= plr.lvlup_threshold:
             plr.score = 0
             plr.lvl += 1
-            plr.counter += 1
-            print(f"Count: {plr.counter}")
-            if plr.lvl <= 4 or plr.lvl >= 6 and plr.lvl != 10 and plr.lvl != 11:
+            if (plr.lvl <= 4) or (plr.lvl >= 6) and plr.lvl != 10 and plr.lvl != 11:
                 level_up()
 
             elif plr.lvl == 5 or plr.lvl == 10:
-                boss_lvl()
-            elif plr.lvl >= 11:
+                if not boss_fight:
+                    boss_fight = True
+                    bosses.empty()
+                    boss_lvl()
+
+            else:
+                boss_fight = False
+
+            if plr.lvl >= 11:
                 game_over = True
 
+
+
         if plr.counter == 2:
-            pow.spawn_percent += 0.2
-            print(f"Powerup percentage: {pow.spawn_percent}")
             plr.counter = 0
-            meteor_spawn_delay -= 100
-            print(f"Delay: {meteor_spawn_delay}")
+            mob_delay_decrease += 50
+            mob_spawn_delay -= mob_delay_decrease
+            print(f"Delay: {mob_spawn_delay}")
+            if plr.lvl == 5 or plr.lvl == 10:
+                mob_spawn_delay = mob_during_boss
 
 
         all_sprites.update()
@@ -596,14 +731,17 @@ while running:
         all_sprites.draw(screen)
 
         status_bar.update(screen)
-
-        draw_text(screen, f"{plr.score}/{plr.lvlup_threshold}", 32, (WIDTH // 4) - 115, 10, YELLOW)
+        if not boss_fight:
+            draw_text(screen, f"{plr.score}/{plr.lvlup_threshold}", 32, (WIDTH // 4) - 115, 10, YELLOW)
 
         draw_lives(screen, WIDTH//2.25, 10, plr.lives, life_icon)
         draw_text(screen, f"{plr.ammo}/{plr.max_ammo}", 32, 60, HEIGHT - 50, IDK)
 
         if plr.reloading:
             draw_text(screen, "RELOADING...", 32, WIDTH // 2, HEIGHT - 40, RED)
+
+        if boss_fight:
+            draw_text(screen, f"{plr.boss_kill}/{plr.total_boss_count}", 32, (WIDTH // 4) - 115, 10, YELLOW)
 
         p.display.flip()
 
